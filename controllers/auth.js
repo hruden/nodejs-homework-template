b0633735de5e8252827f6b6dbc6ce76fab0eec7e
hruden <119438895+hruden@users.noreply.github.com>
2023-10-30 18:56:11 +0200

    start

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,9 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar")
+const path = require("path")
+const fs = require("fs/promises")
+// const Jimp = require("jimp");
 
 const  User  = require("../models/users");
 
@@ -7,6 +11,21 @@ const { SECRET_KEY } = process.env;
 
 const { HttpError, ctrlWrapper } = require("../helpers");
 
+const avatarsDir = path.join(__dirname, "../", "public", "avatars")
+
+const updateAvatar =async(req, res) =>{
+  const {_id} = req.user
+  const {path: tempUpload, originalname, size} = req.file
+  const filename = `${_id}_${size}_${originalname}`;
+  const resultUpload = path.join(avatarsDir, filename)
+  await fs.rename(tempUpload, resultUpload)
+  const avatarURL = path.join("avatars", filename)
+  await User.findByIdAndUpdate(_id, {avatarURL})
+
+  res.status(200).json({
+    avatarURL
+  })
+}
 const register = async (req, res) => {
   const {password, email} = req.body;
   const user = await User.findOne({email})
@@ -14,10 +33,12 @@ const register = async (req, res) => {
     throw HttpError(409, "Email in use");
   }
   const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email)
+
   const newUser = await User.create({
     ...req.body,
     password: hashPassword,
-  
+    avatarURL,
   });
 
   res.status(201).json({
@@ -66,9 +87,11 @@ const logout = async (req, res) => {
   });
 };
 
+
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
+  updateAvatar: ctrlWrapper(updateAvatar)
 };
