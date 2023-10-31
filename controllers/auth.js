1c142f9439265d9868dd6c6771280b9a7b9ec87b
hruden <119438895+hruden@users.noreply.github.com>
2023-10-31 16:39:07 +0200

    fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar")
 const path = require("path")
 const fs = require("fs/promises")
-// const Jimp = require("jimp");
+const Jimp = require("jimp");
 
 const  User  = require("../models/users");
 
@@ -18,6 +18,8 @@ const updateAvatar =async(req, res) =>{
   const {path: tempUpload, originalname, size} = req.file
   const filename = `${_id}_${size}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename)
+  const avatar = await Jimp.read(tempUpload)
+  avatar.resize(250, 250)
   await fs.rename(tempUpload, resultUpload)
   const avatarURL = path.join("avatars", filename)
   await User.findByIdAndUpdate(_id, {avatarURL})
